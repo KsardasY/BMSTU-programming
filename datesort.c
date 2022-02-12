76e8b9623d90c258f129e98aa4b68847664c9521
Andrey Melnikov <KsardasY@yandex.ru>
2022-02-12 16:19:21 +0300

    Update datesort.c

--- datesort.c
+++ datesort.c
@@ -1,20 +1,20 @@
 # include <stdio.h>
 # include <malloc.h>
-# include <math.h>
 
 
-struct Date {
-    int Day, Month, Year;
+union Int32 {
+    int x;
+    unsigned char bytes[4];
 };
 
 
-int key(const struct Date d, int ind) {
-    int k[3] = {d.Day - 1, d.Month - 1, d.Year - 1970};
-    return k[ind];
+unsigned char key(union Int32 c, int ind) {
+    if (ind != 3) return c.bytes[ind];
+    else return c.bytes[3] - 128;
 }
 
 
-void distributionsort(const struct Date *a, struct Date *sd, const int *keys, int m, int n, int ind) {
+void distributionsort(union Int32 *a, union Int32 *sd, const int *keys, int m, int n, int ind) {
     int d[m], k;
     d[0] = keys[0];
     for (int i = 1; i < m; i++) {
@@ -28,13 +28,15 @@ void distributionsort(const struct Date *a, struct Date *sd, const int *keys, in
 }
 
 
-void datesort(struct Date *a, struct Date *sd, int nel) {
-    int len[3] = {31, 12, 61};
-    for (int i = 0; i < 3; i++) {
-        int keys[len[i]];
-        for (int j = 0; j < len[i]; j++) keys[j] = 0;
+void radixsort(union Int32 *a, union Int32 *sd, int nel) {
+    int len = 256;
+    for (int i = 0; i < 4; i++) {
+        int keys[len];
+        for (int j = 0; j < len; j++) keys[j] = 0;
         for (int j = 0; j < nel; j++) keys[key(a[j], i)]++;
-        distributionsort(a, sd, keys, len[i], nel, i);
+        for (int j = 0; j < nel; j++) printf("%d %d\n", a[j].x, key(a[j], i));
+        printf("\n");
+        distributionsort(a, sd, keys, len, nel, i);
         for (int j = 0; j < nel; j++) a[j] = sd[j];
     }
 }
@@ -43,10 +45,10 @@ void datesort(struct Date *a, struct Date *sd, int nel) {
 int main() {
     int n;
     scanf("%d", &n);
-    struct Date a[n], *s = malloc(sizeof(struct Date) * n);
-    for (int i = 0; i < n; i++) scanf("%d %d %d", &a[i].Year, &a[i].Month, &a[i].Day);
-    datesort(a, s, n);
+    union Int32 a[n], *s = malloc(sizeof(union Int32) * n);
+    for (int i = 0; i < n; i++) scanf("%d", &a[i].x);
+    radixsort(a, s, n);
     free(s);
-    for (int i = 0; i < n; i++) printf("%04d %02d %02d\n", a[i].Year, a[i].Month, a[i].Day);
+    for (int i = 0; i < n; i++) printf("%d ", a[i].x);
     return 0;
 }
