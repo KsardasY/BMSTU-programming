87161b80ff0f6cc6781789aa757501c499b20bdd
Andrey Melnikov <KsardasY@yandex.ru>
2022-05-30 15:26:39 +0300

    Update Test.java

--- Programming languages and methods/L3.1/Test.java
+++ Programming languages and methods/L3.1/Test.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         Equation[] a = new Equation[] {
-                new Equation(1, 2, 1), /* dist == 1(2) */
-                new Equation(1, 0, 1), /* dist == 2(13 или 17) */
-                new Equation(1, -3, 2), /* dist == 0 */
+                new Equation(1, 2, 1), /* -1 */
+                new Equation(1, 0, 1), /* Нет корней */
+                new Equation(1, -3, 2), /* 1, 2 */
         };
         Arrays.sort(a);
         for (Equation x : a) System.out.println(x);
