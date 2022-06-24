0f6e2343cce8dfa88706c7f46599006300e8695a
Andrey Melnikov <KsardasY@yandex.ru>
2022-06-24 14:55:58 +0300

    Add files via upload

---
+++ Discrete mathematics/1st module/add.go
@@ -0,0 +1,30 @@
+package main
+
+func add(a, b []int32, p int) []int32 {
+	x := int32(p)
+	c := int32(0)
+	var res []int32
+	if len(a) < len(b) {
+		a, b = b, a
+	}
+	for i := 0; i < len(b); i++ {
+		res = append(res, (a[i]+b[i]+c)%x)
+		c = (a[i] + b[i] + c) / x
+	}
+	for i := len(b); i < len(a); i++ {
+		res = append(res, (a[i]+c)%x)
+		c = (a[i] + c) / x
+	}
+	if c != 0 {
+		res = append(res, c)
+	}
+	return res
+}
+
+func main() {
+	a := []int32{1, 1, 1, 1}
+	b := []int32{0, 0, 1, 0, 1, 1, 1}
+	for _, v := range add(a, b, 2) {
+		print(v, " ")
+	}
+}
