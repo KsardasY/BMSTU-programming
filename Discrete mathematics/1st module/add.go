8f44a13ebd90eabf197467a36bec222765da795a
Andrey Melnikov <KsardasY@yandex.ru>
2022-06-27 23:24:49 +0300

    Update add.go

--- Discrete mathematics/1st module/add.go
+++ Discrete mathematics/1st module/add.go
@@ -3,13 +3,17 @@ package main
 func add(a, b []int32, p int) []int32 {
 	x := int32(p)
 	c := int32(0)
-	var res []int32
+	var (
+		res []int32
+		s   int32
+	)
 	if len(a) < len(b) {
 		a, b = b, a
 	}
 	for i := 0; i < len(b); i++ {
-		res = append(res, (a[i]+b[i]+c)%x)
-		c = (a[i] + b[i] + c) / x
+		s = a[i] + b[i] + c
+		res = append(res, s%x)
+		c = s / x
 	}
 	for i := len(b); i < len(a); i++ {
 		res = append(res, (a[i]+c)%x)
